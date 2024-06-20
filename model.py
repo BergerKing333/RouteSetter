f0a33fb018a5ee1be4d5a4709fa73ba58eb3ac3c
Alex Berg <56053786+BergerKing333@users.noreply.github.com>
2024-06-20 17:03:53 -0500

    Code to scrape kilter route data, and tf model

--- model.py
+++ model.py
@@ -1,16 +1,35 @@
-from ultralytics import YOLO
-import cv2
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
 
-model = YOLO('yolov9c')
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
 
-# results = model.predict("RocaWall1.jpg")
+df = pd.read_csv('training_data.csv')
 
-# image = results.render()
-# image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-# cv2.imshow('image', image)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-ws
-# print(results)
+x = df.drop('rating', axis=1)
+x = x.drop('Unnamed: 0', axis=1)
+y = df['rating']
 
-results = model.train(data='data.yaml', epochs=10, imgsz=640)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.01)
+
+
+model = Sequential([
+    Dense(128, activation='relu', input_shape=(x_train.shape[1],)),
+    Dense(128, activation='relu'),
+    Dense(64, activation='relu'),
+    Dense(32, activation='relu'),
+    Dense(1, activation='linear')
+])
+
+
+model.compile(optimizer=Adam(learning_rate=0.001), loss='mse', metrics=['accuracy'])
+
+history = model.fit(x_train, y_train, epochs=200, batch_size=200, validation_data=(x_test, y_test))
+
+loss, accuracy = model.evaluate(x_test, y_test)
+
+print(model.predict(x_test))
+print(y_test)
